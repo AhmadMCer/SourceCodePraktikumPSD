c24b5e92273a8b8b2e4fbaff8b5de4f346cada35
AhmadMCer <ahmadmcer@gmail.com>
2021-07-16 00:02:52 +0700

    Perbaharuan di class Main

--- src/modul11/Main.java
+++ src/modul11/Main.java
@@ -1,19 +1,27 @@
 package modul11;
 
-import com.sun.xml.internal.bind.v2.model.annotation.Quick;
-
 public class Main {
     public static void main(String[] args) {
-        int[] array = new int[]{12, 4, 8, 5, 7, 3, 22, 4, 5, 13};
-        System.out.println("Array sebelum di sort :");
-        QuickSort.printArray(array);
-        QuickSort.quickSort(array, 0, array.length-1);
+        int[] array = new int[] {12, 4, 8, 5, 7, 3, 22, 4, 5, 13};
+        System.out.println("Quick Sort");
+
+        System.out.print("Sebelum sorting\t: ");
         QuickSort.printArray(array);
 
-        int[] array2 = new int[]{12, 4, 8, 5, 7, 3, 22, 4, 5, 13};
-        System.out.println("Array sebelum di sort :");
+        QuickSort.quickSort(array, 0, array.length - 1);
+
+        System.out.print("Sesudah sorting\t: ");
+        QuickSort.printArray(array);
+
+        int[] array2 = new int[] {12, 4, 8, 5, 7, 3, 22, 4, 5, 13};
+        System.out.println("\nMerge Sort");
+
+        System.out.print("Sebelum sorting\t: ");
         MergeSort.printArray(array2);
-        MergeSort.mergeSort(array2, 0, array2.length-1);
+
+        MergeSort.mergeSort(array2, 0, array2.length - 1);
+
+        System.out.print("Sesudah sorting\t: ");
         MergeSort.printArray(array2);
     }
 }
